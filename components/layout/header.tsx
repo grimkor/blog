7e29c4181fc7a31f2a2f125964d8f80b46a2878d
david <david@grimkor.tech>
2020-04-27 00:14:07 +0100

    added icons

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,19 +1,28 @@
-import { useRouter } from "next/router";
-import { FC } from "react";
+import React, { FC } from "react";
 
 const Link = ({ children, href }) => {
-  const router = useRouter();
   return (
-    <a href={href} className="header__link">
+    <a href={href} className="header__item">
       {children}
     </a>
   );
 };
 
+const Icon = ({ icon, href }) => {
+  return (
+    <a href={href} target="_blank" className={`header__icon--${icon} fa-2x`} />
+  );
+};
+
 const Header: FC = () => (
   <div className="header">
-    <Link href="/">Home</Link>
-    <Link href="/blog">Posts</Link>
+    <div className="header__group">
+      <Link href="/">Home</Link>
+      <Link href="/blog">Posts</Link>
+    </div>
+    <div className="header__group">
+      <Icon href="https://github.com/grimkor" icon="github" />
+    </div>
   </div>
 );
 
