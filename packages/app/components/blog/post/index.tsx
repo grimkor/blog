0e4f8e0ce84217b1fa1c4e69a67917d36aec64d4
david <david@grimkor.tech>
2020-06-30 21:06:07 +0100

    code-formatting

--- packages/app/components/blog/post/index.tsx
+++ packages/app/components/blog/post/index.tsx
@@ -1,20 +1,37 @@
 import React, { FC } from "react";
 import { PostInterface } from "../../../types";
-import ReactMarkdown from "react-markdown";
-import getConfig from 'next/config';
-
-const { serverRuntimeConfig } = getConfig();
-
+import unified from "unified";
+import markdown from "remark-parse";
+import remark2react from "remark-react";
+import remark2rehype from "remark-rehype";
+import highlight from "rehype-highlight";
+import rehype2react from "rehype-react";
+import typescript from "highlight.js/lib/languages/typescript";
+import javascript from "highlight.js/lib/languages/javascript";
 interface PostProps {
   data: PostInterface;
 }
+
+const processor = unified()
+  .use(markdown)
+  .use(remark2rehype)
+  .use(highlight, {
+    languages: {
+      typescript,
+      javascript,
+    },
+    aliases: {
+      javascript: ["tsx"],
+    },
+  })
+  .use(rehype2react, { createElement: React.createElement });
 
 const Post: FC<PostProps> = ({ data }) => {
   return (
     <article className="post">
       <h1 className="--centered">{data.data.title}</h1>
       <h4 className="--centered --subheader">{data.data.date}</h4>
-      <ReactMarkdown source={data.content} />
+      {processor.processSync(data.content).result}
     </article>
   );
 };
--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -2,6 +2,8 @@ import React, { FC } from "react";
 import "../styles/index.scss";
 import { AppProps } from "next/app";
 import Layout from "../components/layout";
+// import "highlight.js/styles/a11y-dark.css";
+import "highlight.js/styles/darcula.css";
 
 const BlogApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
